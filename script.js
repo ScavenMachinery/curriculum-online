f211aecd2eeb144908e828d600765c34bd1093a1
Federico Panopio <panopiofederico@gmail.com>
2025-09-07 10:32:38 +0200

    Versione iniziale del CV

---
+++ script.js
@@ -0,0 +1,14 @@
+export default function Home() {
+    const products = [
+        { id:1, name: "Cuffie Bluetooth", price: 59.99 },
+        { id:1, name: "Cuffie Bluetooth", price: 59.99 },
+        { id:1, name: "Cuffie Bluetooth", price: 59.99 },
+    ];
+
+    return (
+        <main style={{ fontFamily: "Arial, sans-serif", padding: "20px" }}>
+            <h1 style={{ fontSize:"2rem" marginBottom:"1rem!}}>
+        Benvenuto nel mio WaveShaperNode<h1>
+        "}}
+    )
+}
